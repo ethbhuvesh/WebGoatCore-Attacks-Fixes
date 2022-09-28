c3eacd85a2d992fc4b70e87fef8d18a91fa2c6cb
Bhuvesh Gupta <bgupta1@umd.edu>
2022-09-27 22:54:24 -0400

    Fix Phase1 SQLInjection Q1

--- WebGoatCore/Data/BlogResponseRepository.cs
+++ WebGoatCore/Data/BlogResponseRepository.cs
@@ -16,8 +16,13 @@ namespace WebGoatCore.Data
         {
             //TODO: should put this in a try/catch
             // Use EntityFramework FromSQLRaw for faster query
-            var responseBack = _context.BlogResponses.FromSqlRaw(
-                $"INSERT INTO BlogResponses (Author, BlogEntryId, ResponseDate, Contents) VALUES ( '{response.Author}', '{response.BlogEntryId}', '{response.ResponseDate}', '{response.Contents}' ); SELECT * FROM BlogResponses WHERE changes() = 1 AND Id = last_insert_rowid();").ToListAsync();
+            string Author = response.Author;
+            int BlogEntryId = response.BlogEntryId;
+            var ResponseDate = response.ResponseDate;
+            string Contents = response.Contents;
+
+            var responseBack = _context.BlogResponses.FromSqlInterpolated(
+                $"INSERT INTO BlogResponses (Author, BlogEntryId, ResponseDate, Contents) VALUES ( {Author}, {BlogEntryId}, {ResponseDate}, {Contents} ); SELECT * FROM BlogResponses WHERE changes() = 1 AND Id = last_insert_rowid();").ToListAsync();
         }
     }
 }
